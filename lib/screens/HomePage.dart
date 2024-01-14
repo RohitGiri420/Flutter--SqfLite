a8fefa9111b8a981db7046cab869ed31b0ebedc9
Rohit Giri <giri420rohit@gmail.com>
2024-01-14 23:31:24 +0530

    addData fuction is added with ui

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,5 +1,10 @@
+import 'dart:html';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:sqflite1/database/DbHelper.dart';
 import 'package:sqflite1/widgets/UiHelper.dart';
+import 'dart:developer';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -23,12 +28,22 @@ class _HomePageState extends State<HomePage> {
           children: [
             UiHelper.CustomTextField(emailController, "Email"),
             UiHelper.CustomTextField(passController, "Password"),
-            UiHelper.CustomButton(() { }, "Save")
+            UiHelper.CustomButton(() { adddata(emailController.text.toString(), passController.text.toString()); }, "Save")
           ],
 
         ),
       ),
 
     );
+  }
+
+  adddata(String email,String password){
+    if(email=="" && password == ""){
+      return 0;
+    }
+
+    else{
+      DbHelper().addData(email, password);
+    }
   }
 }
